02806dbfce16bd1897c59a62e5cccece2e7ea7c0
Duong Huy Phuoc Chu <chuduonghuyphuoc@gmail.com>
2024-11-06 17:03:07 +0700

    commit 2

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ class Config:
     DEBUG = True
     DATABASE_URI = (
         "DRIVER={ODBC Driver 17 for SQL Server};"
-        "SERVER=DESKTOP-HQ4AVAA\SQLEXPRESS;"  # Replace with your server name
+        "SERVER=DESKTOP-HQ4AVAA\SQLEXPRESS;"  
         "DATABASE=ORDER_STATUS;"
-        "Trusted_Connection=yes;"# Replace with the database name from order_Status.sql
-        # Replace with your SQL Server password
+        "Trusted_Connection=yes;"
+       
         
     )
